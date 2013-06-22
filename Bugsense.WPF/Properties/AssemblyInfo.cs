d9c4e182e5b81d440676c9668e46dab41a91aa53
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-06-22 22:07:19 +0200

    Strong signing assembly

--- Bugsense.WPF/Properties/AssemblyInfo.cs
+++ Bugsense.WPF/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Bugsense.WPF")]
 [assembly: AssemblyDescription("Adds support for sending crash reports to bugsense")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Rebtel Networks AB")]
+[assembly: AssemblyCompany("Vidstige")]
 [assembly: AssemblyProduct("Bugsense.WPF")]
-[assembly: AssemblyCopyright("Copyright © Rebtel Networks AB 2012")]
+[assembly: AssemblyCopyright("Copyright © Vidstige AB 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
