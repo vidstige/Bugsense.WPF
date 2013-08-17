b507b3a07369f6330068c44c24a9c74189507299
Samuel Carlsson <samuel.carlsson@gmail.com>
2013-08-17 16:10:23 +0200

    Changing to MSTest.

--- Bugsense.WP.tests/Given_Exception_is_thrown.cs
+++ Bugsense.WP.tests/Given_Exception_is_thrown.cs
@@ -1,9 +1,19 @@
-﻿using NUnit.Framework;
+﻿using Bugsense.WPF;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Net;
 
 namespace Bugsense.WP.tests
 {
-    [TestFixture]
+    [TestClass]
     public class Given_Exception_is_thrown
     {
+        [TestMethod]
+        public void Foobar()
+        {
+            var webRequestCreator = new Mock<IWebRequestCreate>();
+            var errorSender = new ErrorSender(null, null, webRequestCreator.Object);
+            errorSender.SendOrStore(new BugSenseRequest(new BugSenseEx(), new AppEnvironment()));
+        }
     }
 }
--- Bugsense.WPF/Properties/AssemblyInfo.cs
+++ Bugsense.WPF/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -33,3 +34,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
+
+// Allow the test assembly to access internal classes
+[assembly: InternalsVisibleTo("Bugsense.WP.tests")]
